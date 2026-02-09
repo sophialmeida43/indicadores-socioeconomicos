be5c6f2ba7334b3c38d673e9cbcd3275c55d3760
Sophia Almeida <sophia.almeida.work@gmail.com>
2026-02-08 22:57:14 -0300

    corigindo o nome do repo.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Indicadores-Socioecon-micos',
+  base: '/indicadores-socioeconomicos/',
 })
